3c4e02172c55704c63eb1ec8d03a52a3931fd64f
David Adams <russianid1@gmail.com>
2013-07-04 23:17:12 +0000

    gemfile edits

---
+++ vendor/bundle/ruby/1.9.1/gems/nokogiri-1.6.0/ports/x86_64-linux-gnu/libxslt/1.1.26/lib/xsltConf.sh
@@ -0,0 +1,7 @@
+#
+# Configuration file for using the xslt library
+#
+XSLT_LIBDIR="-L/home/action/brilliant/vendor/bundle/ruby/1.9.1/gems/nokogiri-1.6.0/ports/x86_64-linux-gnu/libxslt/1.1.26/lib"
+XSLT_LIBS="-lxslt  -L/home/action/brilliant/vendor/bundle/ruby/1.9.1/gems/nokogiri-1.6.0/ports/x86_64-linux-gnu/libxml2/2.8.0/lib -lxml2 -lz -lm -lm"
+XSLT_INCLUDEDIR="-I/home/action/brilliant/vendor/bundle/ruby/1.9.1/gems/nokogiri-1.6.0/ports/x86_64-linux-gnu/libxslt/1.1.26/include"
+MODULE_VERSION="xslt-1.1.26"
