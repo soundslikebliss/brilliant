7f11377e4e6f3ba68fac44b5ea401e7ed93d6ec4
David Adams <russianid1@gmail.com>
2013-07-04 21:44:17 +0000

    add paperclip functionality

--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,3 +1,9 @@
 class Photo < ActiveRecord::Base
-  # attr_accessible :title, :body
+  attr_accessible :image
+  
+  validates_attachment :image, presence: true,
+                            content_type: { content_type: ['image/jpeg', 'image/jpg', 'image/png', 'image/gif'] },
+                            size: { less_than: 5.megabytes }
+  belongs_to :user
+  has_attached_file :image, styles: { medium: "320x240>"}
 end
